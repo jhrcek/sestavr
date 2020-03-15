d20add2c94f024994b56939d649b21c0b3d5d866
Jan Hrček <jhrcek@globalwebindex.com>
2020-03-15 19:01:56 +0100

    Proper http status code on constraint failure

--- src/Server.hs
+++ src/Server.hs
@@ -13,23 +13,19 @@ module Server
 where
 
 import Api (SestavrAPI, sestavrApi)
-import Control.Monad.IO.Class (liftIO)
+import Control.Exception.Safe (catch, throwM)
+import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Logger (runStderrLoggingT)
 import Data.ByteString (ByteString)
 import Data.ByteString.Lazy.Char8 (pack)
 import Data.FileEmbed (embedFile)
 import qualified Data.Text as Text
 import Database.Persist.Class (get, insertEntity, selectList)
-import Database.Persist.Sqlite
-  ( ConnectionPool,
-    createSqlitePool,
-    runMigration,
-    runSqlPersistMPool,
-    runSqlPool,
-  )
+import Database.Persist.Sql (SqlPersistM)
+import Database.Persist.Sqlite (ConnectionPool, createSqlitePool, runMigration, runSqlPersistMPool, runSqlPool)
 import Database.Persist.Types (Entity)
+import Database.Sqlite (Error (ErrorConstraint), SqliteException, seError)
 import Model (Exercise, Lesson, Position, PositionId, Target, migrateAll)
-import Network.Wai
 import qualified Network.Wai.Handler.Warp as Warp
 import Servant
 
@@ -60,6 +56,7 @@ apiServer pool =
     --
     :<|> createTarget
   where
+    runPool :: MonadIO m => SqlPersistM a -> m a
     runPool action = liftIO $ runSqlPersistMPool action pool
     --
     getPosition :: PositionId -> Handler Position
@@ -82,8 +79,13 @@ apiServer pool =
     getTargets = runPool $ selectList [] []
     --
     createTarget :: Target -> Handler (Entity Target)
-    createTarget =
-      runPool . insertEntity
+    createTarget target =
+      runPool (insertEntity target)
+        `catch` ( \(e :: SqliteException) ->
+                    case seError e of
+                      ErrorConstraint -> throwError $ err409 {errBody = "Target with this name already exists"}
+                      _ -> throwM e
+                )
 
 getIndex :: Handler ByteString
 getIndex = pure indexHtml
