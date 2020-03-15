42c70c225ec485a0b6f089ed076a0abd6603a84f
Jan Hrček <jhrcek@globalwebindex.com>
2020-03-15 09:18:02 +0100

    Implement skeleton of Browser.application

---
+++ Makefile
@@ -0,0 +1,14 @@
+script=dist/main.js
+
+front:
+	cd client && elm make src/Main.elm --optimize --output=$(script)
+
+mini: front
+	uglifyjs client/$(script) --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=client/$(script)
+
+run: mini
+	stack run
+
+clean:
+	rm client/$(script)
+	stack clean
--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,5 +1,56 @@
 module Main exposing (main)
 
+import Browser exposing (Document, UrlRequest)
+import Browser.Navigation exposing (Key)
 import Html
+import Url exposing (Url)
 
-main = Html.text "hello"
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = UrlRequest
+        , onUrlChange = UrlChange
+        }
+
+
+type alias Model =
+    { navKey : Key }
+
+
+type Msg
+    = NoOp
+    | UrlRequest UrlRequest
+    | UrlChange Url
+
+
+init : () -> Url -> Key -> ( Model, Cmd msg )
+init _ url key =
+    ( { navKey = key }, Cmd.none )
+
+
+view : Model -> Document msg
+view model =
+    { title = "sestavr", body = [] }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UrlRequest urlReq ->
+            ( model, Cmd.none )
+
+        UrlChange url ->
+            ( model, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
